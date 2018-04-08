3c72bb8a93d1f0ba95d8fa31df1a2e037230bba2
Michael Boroski <mjboroski@gmail.com>
2018-04-08 02:27:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,39 +15,27 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if board.include?("X"||"O")==false
-    return false
-  elsif
-    WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
-
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X")||(position_1 == "O" && position_2 == "O" && position_3 == "O")
-        return win_combination
-      end
-    end
-  elsif full?(board)==true
-    return false
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
 def full?(board)
-  board.all?{|index| index=='X'||index=="O"}
+  board.all?{|token| token == "X" || token == "O"}
 end
 
 def draw?(board)
-  full?(board)&&!won?(board)
+  full?(board) && !won?(board)
 end
 
 def over?(board)
-  won?(board)==true||draw?(board)==true
+  won?(board) || full?(board)
 end
 
 def winner(board)
-
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
 end
