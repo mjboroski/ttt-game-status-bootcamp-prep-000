16682ba05d6a3993920d2d2480a6cd2430686620
Michael Boroski <mjboroski@gmail.com>
2018-04-08 02:23:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,6 @@ def won?(board)
 
       if (position_1 == "X" && position_2 == "X" && position_3 == "X")||(position_1 == "O" && position_2 == "O" && position_3 == "O")
         return win_combination
-      else
-        false
       end
     end
   elsif full?(board)==true
